fec864c6561ef04b37d5901976a18d8ad7547095
siag7072 <siag7072@colorado.edu>
2019-06-04 18:54:11 +0000

    added new funcitons updated instruction comments

--- code/BST.cpp
+++ code/BST.cpp
@@ -33,18 +33,31 @@ bool BST::Contains(shared_ptr<bt_node> t, int data){
   return ret;
 }
 
-shared_ptr<bt_node> BST::GetNode(shared_ptr<bt_node> t, int data){
+shared_ptr<bt_node> BST::GetNode(shared_ptr<bt_node> subt, int data){
   shared_ptr<bt_node> ret;
   // Your code here
   return ret;
 }
 
-int BST::Size(shared_ptr<bt_node> t){
+int BST::Size(shared_ptr<bt_node> subt){
   int ret;
   // Your code here
   return ret;
 }
 
-void BST::ToArray(shared_ptr<bt_node> t, int arr[]){
+void BST::ToArray(shared_ptr<bt_node> subt, vector<int>& arr){
   // Your code here
+  // eventually fill in arr ex. arr[0] = 1;
+}
+
+// This function is implemented for you
+// Returns the root pointer
+shared_ptr<bt_node> BST::GetRoot(){
+  return root_ptr_;
+}
+
+// This function is implemented for you
+// sets a given pointer as the top pointer
+void BST::SetRoot(shared_ptr<bt_node> root_ptr){
+  root_ptr_ = root_ptr;
 }
--- code/BST.h
+++ code/BST.h
@@ -22,10 +22,10 @@ using namespace std;
 class BST {
 public:
   // constructor, initialize class variables and pointers here if need.
-  // Initially set top pointer to a null shared_ptr of node
+  // Initially set root_ptr_ to a null shared_ptr of node
   BST();
 
-  //deconstructor, if you are using shared_pointers you don't need to keep track of
+  // deconstructor, if you are using shared_pointers you don't need to keep track of
   // freeing memory and you can leave this blank
   ~BST();
 
@@ -34,7 +34,7 @@ public:
   shared_ptr<bt_node> InitNode(int data);
 
   // insert places the new_node in a proper location in the tree
-  // indicated by top, while obeying the invariant. On return, *top
+  // while obeying the invariant. On return, root_ptr_
   // points to the root of the tree.
   void Insert(shared_ptr<bt_node> new_node);
 
@@ -43,36 +43,50 @@ public:
   void Insert_data(int data);
 
   // remove removes a node from the tree whose data value matches the
-  // input. On return, *top points to the root of the tree. If no node
-  // in the tree contains the given data, this function has no effect.
+  // input. If no node in the tree contains the given data, this function
+  // has no effect.
   void Remove(int data);
 
   // contains returns true if any node in the subtree pointed to by t
   // contains the given data value, false otherwise.
-  bool Contains(shared_ptr<bt_node> t, int data);
+  bool Contains(shared_ptr<bt_node> subt, int data);
 
-  // get_node searches through the subtree pointed to by t for a node that
+  // get_node searches through the subtree pointed to by subt for a node that
   // contains the given data value. If such a node is found, a pointer
   // to it is returned. Otherwise this function returns NULL.
-  shared_ptr<bt_node> GetNode(shared_ptr<bt_node> t, int data);
+  shared_ptr<bt_node> GetNode(shared_ptr<bt_node> subt, int data);
 
-  // size returns the number of nodes in the subtree pointed to by t. If
+  // size returns the number of nodes in the subtree pointed to by subt. If
   // the tree is empty (t is NULL), it returns zero.
-  int Size(shared_ptr<bt_node> t);
+  int Size(shared_ptr<bt_node> subt);
 
   // to_array fills an integer array to reflect the contents of the
-  // subtree pointed to by t. The filled array will be the same as the
+  // subtree pointed to by subt. Size of the filled array will be the same as the
   // subtree's size (found with the size() function), and the order of
   // the array elements are the same that is found during an inorder
   // traversal of the subtree.
   //
   // Note: the array is created and sized before this function is
-  // called. You will have at least size(top) elements available, so you
+  // called. You will have at least Size(root_ptr_) elements available, so you
   // may safely index [0..size(t)-1].
-  void ToArray(shared_ptr<bt_node> t, int arr[]);
+  // vector<int> is a cpp class which defines a vector of type int.
+  // for example you can define a vector of size 10 like:
+  // vector<int> my_vec(10);
+  // and pass that vector by reference to ToArray function to be filled like:
+  // ToArray(somesharedpointer, myvec)
+  void ToArray(shared_ptr<bt_node> subt, vector<int>& arr);
+
+  // This function is implemented for you
+  // Returns the root pointer
+  shared_ptr<bt_node> GetRoot();
+
+  // This function is implemented for you
+  // sets a given pointer as the top pointer
+  void SetRoot(shared_ptr<bt_node> root_ptr);
 
 private:
-  shared_ptr<bt_node> top_ptr_;
+  // this pointer always will point to root of the tree
+  shared_ptr<bt_node> root_ptr_;
   // you can add add more private member variables and member functions here if you need
 };
 
